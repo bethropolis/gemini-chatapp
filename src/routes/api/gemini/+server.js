30178f7520b7e4cb4b9b02e5e9e2c8d75655df65
Bethuel <bethropolis@gmail.com>
2024-02-02 23:42:52 +0000

    goo

---
+++ src/routes/api/gemini/+server.js
@@ -0,0 +1,27 @@
+import { json } from '@sveltejs/kit';
+import { env } from '$env/dynamic/private';
+import Gemini from "gemini-ai";
+
+
+const gemini = new Gemini(env.PRIVATE_API_KEY);
+
+export async function POST({ request }) {
+  const { query } = await request.json();
+
+  try {
+    let text = await gemini.ask(query, {
+      stream: console.log,
+    });
+
+
+    return json({ text }, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return json({ error: error.message }, { status: 500 });
+  }
+}
