850938a5bcc331288f48edff086dc8cbfcfb2bc5
ruxartic <155459652+ruxartic@users.noreply.github.com>
2024-02-04 04:26:00 +0000

    pwa support

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,46 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()
+		, VitePWA({
+			injectRegister: 'auto',
+			registerType: 'autoUpdate',
+			workbox: {
+			  globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+			},
+			manifest: {
+			  name: 'gemini chat',
+			  short_name: 'gemini',
+			  description: 'a chat app to converse with the gemini pro model',
+			  theme_color: '#ffffff',
+			  icons: [
+				{
+				  src: 'web/icon-192.png',
+				  sizes: '192x192',
+				  type: 'image/png'
+				},
+				{
+				  src: 'web/icon-192-maskable.png',
+				  sizes: '192x192',
+				  type: 'image/png',
+				  purpose: 'maskable'
+				},
+				{
+				  src: 'web/icon-512.png',
+				  sizes: '512x512',
+				  type: 'image/png'
+				},
+				{
+				  src: 'web/icon-512-maskable.png',
+				  sizes: '512x512',
+				  type: 'image/png',
+				  purpose: 'maskable'
+				}
+			  ]
+			}
+		  })
+	]
 });
